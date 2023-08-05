2192e578393919704a912e98d0c832f197239ce6
wiliangulini <gulini.dev@gmail.com>
2023-08-05 04:52:46 -0300

    select track com e sem stems, e tbm select single or album trackWithStems

--- src/app/produtores/produtores.component.ts
+++ src/app/produtores/produtores.component.ts
@@ -1,6 +1,15 @@
-import {AfterViewChecked, AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {ScrollService} from "../service/scroll.service";
+import {empty} from "rxjs";
 
 
 @Component({
@@ -12,7 +21,30 @@ export class ProdutoresComponent implements OnInit, AfterViewInit, AfterViewChec
 
   form: FormGroup;
 
-  producer: string = 'trackNoStems';
+  @ViewChild('CWE') CWE: any;
+
+  rules: string = '';
+  producer: string = '';
+  selectOption: string = '';
+  rulesVal: any;
+
+  oneOrAlbum: Array<any> = [
+    {value: '1 Track com 4 stems', viewValue: '1 Track com 4 stems'},
+    {value: '10 Tracks com 30 stems', viewValue: '10 Tracks com 30 stems'},
+  ]
+  valueTrack: Array<any> = [
+    {value: 'trackNoStems', viewValue: 'Track sem Stems'},
+    {value: 'trackWithStems', viewValue: 'Track com Stems'},
+  ]
+  rulesTrackStems: Array<any> = [
+    {value: '10Tracks', viewValue: 'Nesta opção você produtor poderá fazer upload de até dez tracks sem stems.'},
+    {
+      value: '5Tracks',
+      viewValue: 'Nesta opção você produtor poderá fazer upload de uma track completa com mais quatro stems, totalizando em cinco uploads.'
+    },
+    { value: '10Track30Stems', viewValue: 'Nesta opção você produtor poderá fazer upload de até dez tracks completas com mais trinta stems ( no caso quatro stems por track ), totalizando em quarenta uploads.' },
+  ];
+
 
   constructor(
     private scrollService: ScrollService,
@@ -20,8 +52,8 @@ export class ProdutoresComponent implements OnInit, AfterViewInit, AfterViewChec
     private cdRef: ChangeDetectorRef,
   ) {
     this.form = this.fb.group({
-      track_stems: [],
-      // trackWithStems: [],
+      track_stems: this.producer,
+      selectOption: this.selectOption,
       nome: [],
       sobrenome: [],
       artista_banda: [],
@@ -47,23 +79,37 @@ export class ProdutoresComponent implements OnInit, AfterViewInit, AfterViewChec
   }
 
   changeTrack(elm: any): void {
-    console.log(elm);
+    (elm.value == 'trackWithStems' || elm == 'trackWithStems' || elm.value == 'trackNoStems' || elm == 'trackNoStems') ? this.CWE.nativeElement.style.display = 'inline-grid' : empty();
+    let card: any;
+    this.rulesTrackStems.forEach((e: any, i: number): void => {
+      if (e.value === '10Tracks' && (elm.value == 'trackNoStems' || elm == 'trackNoStems')) {
+        card = document.getElementById('card');
+        card.style.height = '62px';
+        card.style.width = 'auto';
+        this.rules = e.viewValue;
+        this.rulesVal = i;
+      } else if (e.value === '5Tracks' && (elm.value == 'trackWithStems' || elm == 'trackWithStems')) {
+        card = document.getElementById('card');
+        card.style.height = '150px';
+        this.rules = e.viewValue;
+        this.rulesVal = i;
+      }
+    })
 
-    let collapse: any = document.getElementById('collapseWidthExample');
-
-    if(elm.value == 'trackWithStems') {
-      console.log(collapse);
-      console.log(collapse.className);
-
-    } else if(elm.value == 'trackNoStems') {
-      console.log(collapse.className);
-      setTimeout(() => {
-        collapse.classList.remove('show');
-      }, 2000)
-    }
   }
 
-  uploadFile() {
+  optionSelect(event: string): void {
+    let spanRules: any = document.getElementById('rules1');
+    this.rulesTrackStems.forEach((e: any): void => {
+      if (e.value === '10Track30Stems' && event === '10 Tracks com 30 stems') {
+        spanRules.innerText = e.viewValue;
+      } else if (e.value === '5Tracks' && event === '1 Track com 4 stems') {
+        spanRules.innerText = e.viewValue;
+      }
+    });
+  }
+
+  uploadFile(): void {
     const $: any = document.querySelector.bind(document);
 
     let previewFile = $('.uploaded-files');
@@ -77,16 +123,15 @@ export class ProdutoresComponent implements OnInit, AfterViewInit, AfterViewChec
       divPreview.style.display = 'flex';
       let arrayUpload: FileList = e.target.files;
 
-      for(let i: number = 0; i < arrayUpload.length; i++) {
+      for (let i: number = 0; i < arrayUpload.length; i++) {
 
         let fileItem: any = divPreview.cloneNode(true);
         i > 0 ? showFile.append(fileItem) : console.log(fileItem);
-        let s: number = arrayUpload[i].size/1000000;
+        let s: number = arrayUpload[i].size / 1000000;
         let size = s.toFixed(1);
         previewFile.innerText = arrayUpload[i].name;
         previewSize.innerText = size + 'MB';
       }
     };
   }
-
 }
