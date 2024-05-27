9fa3283587bc674fa1b6dcd00e03d7e7132d2cec
wiliangulini <gulini.dev@gmail.com>
2024-05-27 12:07:38 -0300

    player wavesurfer install

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Musica, MusicasService } from '../musicas/musicas.service';
+import WaveSurfer from 'wavesurfer.js';
+import Minimap from 'wavesurfer.js/dist/plugins/minimap';
+
 
 @Component({
   selector: 'app-player',
@@ -8,16 +11,48 @@ import { Musica, MusicasService } from '../musicas/musicas.service';
 })
 export class PlayerComponent implements OnInit {
 
+  ws!: WaveSurfer;
 
   arrMusica: Musica[] = [];
 
   constructor(
     private musicService: MusicasService,
-  ) { }
+  ) {
+
+  }
 
   ngOnInit(): void {
     this.musicService.listMusic().subscribe((data: any) => {
       console.log(data);
+    })
+
+    this.ws = WaveSurfer.create({
+      container: '#waveform',
+      waveColor: 'rgb(200, 0, 200)',
+      progressColor: 'rgb(100, 0, 100)',
+      url: '../../assets/videos/Vibe_Shisui.mp3',
+      minPxPerSec: 100,
+      hideScrollbar: true,
+      autoCenter: false,
+      mediaControls: true,
+      plugins: [
+        // Register the plugin
+        Minimap.create({
+          height: 20,
+          waveColor: '#ddd',
+          progressColor: '#fff',
+          // the Minimap takes all the same options as the WaveSurfer itself
+        }),
+      ],
+    })
+
+    this.playAudio();
+  }
+
+  playAudio() {
+
+    this.ws.on('interaction', () => {
+      this.ws.play();
     })
   }
 
@@ -29,6 +64,10 @@ export class PlayerComponent implements OnInit {
   }
   copiarLink(elm: any) {
     console.log(elm);
+  }
+
+  baixarAmostra(i: any) {
+    console.log(i);
   }
 
 }
