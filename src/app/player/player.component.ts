64d7a9ac8c736410a2d63831a829af7cfbf5821c
wiliangulini <gulini.dev@gmail.com>
2024-06-04 09:48:43 -0300

    ajuste no zoom do player

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterContentInit, Component, OnInit} from '@angular/core';
 import { Musica, MusicasService } from '../musicas/musicas.service';
 import WaveSurfer from 'wavesurfer.js';
 import Minimap from 'wavesurfer.js/dist/plugins/minimap';
@@ -9,7 +9,7 @@ import Minimap from 'wavesurfer.js/dist/plugins/minimap';
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent implements OnInit, AfterContentInit {
 
   arrMusica: Musica[] = [];
 
@@ -20,19 +20,21 @@ export class PlayerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.musicService.listMusic().subscribe((data: any) => {
+    this.musicService.listMusic().subscribe((data: any): void => {
       console.log(data);
-    })
-
-    const ws = WaveSurfer.create({
-      container: document.body,
+    });
+  }
+  
+  ngAfterContentInit(): void {
+    const ws: WaveSurfer = WaveSurfer.create({
+      container: '#waveform',
       waveColor: 'rgb(200, 0, 200)',
       progressColor: 'rgb(100, 0, 100)',
       url: '../../assets/videos/Vibe_Shisui.mp3',
       minPxPerSec: 100,
       dragToSeek: true,
     })
-
+    
     // const ws = WaveSurfer.create({
     //   container: document.body,
     //   waveColor: 'rgb(200, 0, 200)',
@@ -57,49 +59,54 @@ export class PlayerComponent implements OnInit {
     //     }),
     //   ],
     // });
-
+    
     // ws.on('interaction', () => {
     //   ws.play();
     // });
-
-    ws.once('decode', () => {
+    
+    ws.once('decode', (): void => {
       const slider: any = document.querySelector('#zoom');
       console.log(slider);
-
-      slider.addEventListener('input', (e: any) => {
+      
+      slider.addEventListener('input', (e: any): void => {
         console.log(e);
         const minPxPerSec = e.target.valueAsNumber
         ws.zoom(minPxPerSec)
       })
     })
-
+    
     const playButton: any = document.querySelector('#play');
     const forwardButton: any = document.querySelector('#forward');
     const backButton: any = document.querySelector('#backward');
-
-    ws.once('decode', () => {
+    
+    ws.once('decode', (): void => {
       document.querySelectorAll('input[type="checkbox"]').forEach((input: any) => {
-        input.onchange = (e: any) => {
+        input.onchange = (e: any): void => {
           ws.setOptions({
             [input.value]: e.target.checked,
           })
         }
-      })
-
-      playButton.onclick = () => {
-        ws.playPause()
+      });
+      
+      playButton.onclick = (): void => {
+        ws.playPause();
       }
-
-      forwardButton.onclick = () => {
-        ws.skip(5)
+      
+      forwardButton.onclick = (): void => {
+        ws.skip(5);
       }
-
-      backButton.onclick = () => {
-        ws.skip(-5)
+      
+      backButton.onclick = (): void => {
+        ws.skip(-5);
       }
-    })
+    });
+    
+    setTimeout(() => {
+      const scroll: any = document.querySelector('.scroll');
+      console.log(scroll);
+    }, 500);
   }
-
+  
   curtir(e: any) {
     console.log(e);
   }
