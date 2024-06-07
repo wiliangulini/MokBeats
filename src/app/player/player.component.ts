cd6ba043899c416ef4ccb4c7660d2836a8235de5
wiliangulini <gulini.dev@gmail.com>
2024-06-07 13:06:46 -0300

    ajustes no player, time da musica corresponde conforme a musica é tocada e a duration é exibida tbm

--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,7 +1,6 @@
 import {AfterContentInit, Component, OnInit} from '@angular/core';
-import { Musica, MusicasService } from '../musicas/musicas.service';
+import {Musica, MusicasService} from '../musicas/musicas.service';
 import WaveSurfer from 'wavesurfer.js';
-import Minimap from 'wavesurfer.js/dist/plugins/minimap';
 
 
 @Component({
@@ -26,14 +25,11 @@ export class PlayerComponent implements OnInit, AfterContentInit {
   }
   
   ngAfterContentInit(): void {
-    const audio = new Audio();
-    audio.src = '../../assets/videos/Vibe_Shisui.mp3';
     
     const ws = WaveSurfer.create({
       container: "#waveform",
       waveColor: '#2f1ef1',
       progressColor: '#007BFF',
-      media: audio,
       url: '../../assets/videos/Vibe_Shisui.mp3',
       minPxPerSec: 10.6,
       hideScrollbar: true,
@@ -41,21 +37,31 @@ export class PlayerComponent implements OnInit, AfterContentInit {
       height: 100,
       dragToSeek: true,
       // plugins: [
-      //   // Register the plugin
+      //   // Registrar o plugin
       //   Minimap.create({
       //     height: 20,
       //     waveColor: '#ddd',
       //     progressColor: '#fff',
-      //     // the Minimap takes all the same options as the WaveSurfer itself
+      //     // o Minimapa tem todas as mesmas opções do próprio WaveSurfer
       //   }),
       // ],
     });
     
-    document.body.appendChild(audio);
+    const formatTime = (seconds: any) => {
+      const minutes = Math.floor(seconds / 60)
+      const secondsRemainder = Math.round(seconds) % 60
+      const paddedSeconds = `0${secondsRemainder}`.slice(-2)
+      return `${minutes}:${paddedSeconds}`
+    }
     
     const playButton: any = document.querySelector('#play');
     const backButton: any = document.querySelector('#backward');
     const forwardButton: any = document.querySelector('#forward');
+    const timeEl: any = document.querySelector('#time');
+    const durationEl: any = document.querySelector('#duration');
+    
+    ws.on('decode', (duration: any) => (durationEl.textContent = formatTime(duration)))
+    ws.on('timeupdate', (currentTime: any) => (timeEl.textContent = formatTime(currentTime)))
     
     playButton.onclick = (): void => {
       ws.playPause();
@@ -68,7 +74,6 @@ export class PlayerComponent implements OnInit, AfterContentInit {
     backButton.onclick = (): void => {
       ws.skip(-5);
     }
-    
     
   }
   
